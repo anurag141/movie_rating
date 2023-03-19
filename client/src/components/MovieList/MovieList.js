439a3fb57093cd168d7f0be96fec5c6ff9e21e6b
anurag141 <kumawatkct@gmail.com>
2023-03-19 12:09:05 +0530

    mongoose add

--- client/src/components/MovieList/MovieList.js
+++ client/src/components/MovieList/MovieList.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Movie from '../Movie/Movie';
 import axios from 'axios';
 
 async function fetchMovie() {
-    const result = await axios.get("http://localhost:9000/mpvies");
-    return result.data;
+	const result = await axios.get("http://localhost:9000/movies");
+	return result.data;
 }
 
 function showMovie(movie) {
-    return (
-        <Movie movie={ movie } /> 
-    )
+	return (
+		<Movie movie={ movie } /> 
+	)
 }
 
 function MovieList() {
-    return (
-        <div>
-            {movies.map(showMovie)}
-        </div>
-    )
+	const [movies, setMovies] = useState([]);
+	async function postRenderhandler(){
+		const moviesFromServer = await fetchMovie();
+		setMovies(moviesFromServer);
+	}
+	function postRenderhandlerNonAsync() {
+		postRenderhandler();
+	}
+
+	useEffect(postRenderhandlerNonAsync, []);
+
+	return (
+		<div>
+			{movies.map(showMovie)}
+		</div>
+	)
 }
 
 export default MovieList;
